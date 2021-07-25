3b12e51bdd58d781fc277d908854d6306367bf93
Ricardo <ricarditomontserrat@gmail.com>
2021-07-26 00:48:37 +0200

    EngineTest//v0.1 Basic platformer

--- js/main.js
+++ js/main.js
@@ -1,49 +1,184 @@
 import * as Phaser from 'phaser';
+// noinspection JSFileReferences
+import images from '../img/*.png'
+
+const WIN_WIDTH = 800;
+const WIN_HEIGHT = 600;
 
 const config = {
-  type: Phaser.AUTO,
-  width: 800,
-  height: 600,
+  type: Phaser.CANVAS,
+  width: WIN_WIDTH,
+  height: WIN_HEIGHT,
   physics: {
     default: 'arcade',
     arcade: {
-      gravity: { y: 200 }
+      gravity: { y: 300 },
+      debug: false
     }
   },
   scene: {
     preload: preload,
-    create: create
+    create: create,
+    update: update
   }
 };
 
+// noinspection JSUnusedLocalSymbols
 const game = new Phaser.Game(config);
 
-function preload ()
-{
-  this.load.setBaseURL('http://labs.phaser.io');
+const IMG_SKY = 'sky';
+const IMG_GROUND = 'ground';
+const IMG_STAR = 'star';
+const IMG_BOMB = 'bomb';
+const SPRITE_DUDE = 'dude';
 
-  this.load.image('sky', 'assets/skies/space3.png');
-  this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-  this.load.image('red', 'assets/particles/red.png');
+function preload()
+{
+  this.load.image(IMG_SKY, images.sky);
+  this.load.image(IMG_GROUND, images.platform);
+  this.load.image(IMG_STAR, images.star);
+  this.load.image(IMG_BOMB, images.bomb);
+  this.load.spritesheet(SPRITE_DUDE, images.dude, { frameWidth: 32, frameHeight: 48 });
 }
 
-function create ()
+function createPlatforms(engine)
 {
-  this.add.image(400, 300, 'sky');
+  const platforms = engine.physics.add.staticGroup();
 
-  const particles = this.add.particles('red');
+  platforms.create(400, 568, IMG_GROUND).setScale(2).refreshBody();
 
-  const emitter = particles.createEmitter({
-    speed: 100,
-    scale: { start: 1, end: 0 },
-    blendMode: 'ADD'
+  platforms.create(600, 400, IMG_GROUND);
+  platforms.create(50, 250, IMG_GROUND);
+  platforms.create(750, 220, IMG_GROUND);
+
+  return platforms;
+}
+
+function createPlayer(engine)
+{
+  const player = engine.physics.add.sprite(100, 450, SPRITE_DUDE);
+
+  player.setBounce(0.2);
+  player.setCollideWorldBounds(true);
+
+  engine.anims.create({
+    key: 'left',
+    frames: engine.anims.generateFrameNumbers(SPRITE_DUDE, { start: 0, end: 3 }),
+    frameRate: 5,
+    repeat: -1
   });
 
-  const logo = this.physics.add.image(400, 100, 'logo');
+  engine.anims.create({
+    key: 'turn',
+    frames: [{ key: SPRITE_DUDE, frame: 4 }],
+    frameRate: 20
+  });
 
-  logo.setVelocity(100, 200);
-  logo.setBounce(1, 1);
-  logo.setCollideWorldBounds(true);
+  engine.anims.create({
+    key: 'right',
+    frames: engine.anims.generateFrameNumbers(SPRITE_DUDE, { start: 5, end: 8 }),
+    frameRate: 5,
+    repeat: -1
+  });
 
-  emitter.startFollow(logo);
+  return player;
+}
+
+function createStars(engine)
+{
+  const stars = engine.physics.add.group({
+    key: IMG_STAR,
+    repeat: 11,
+    setXY: { x: 12, y: 0, stepX: 70 }
+  });
+
+  stars.children.iterate(child => child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8)));
+
+  engine.physics.add.overlap(player, stars, (player, star) =>
+  {
+    star.disableBody(true, true);
+    scoreText.setText('Score: ' + (score += 10));
+
+    if (stars.countActive(true) === 0)
+    {
+      stars.children.iterate(child => child.enableBody(true, child.x, 0, true, true));
+
+      const x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+
+      const bomb = bombs.create(x, 16, 'bomb');
+      bomb.setBounce(1);
+      bomb.setCollideWorldBounds(true);
+      bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+    }
+
+  }, null, this);
+
+  return stars;
+}
+
+let cursors, player;
+
+let score = 0;
+let scoreText;
+
+let fpsText;
+let gameOver;
+let bombs;
+
+function create()
+{
+  const engine = this;
+
+  engine.add.image(0, 0, IMG_SKY).setOrigin(0, 0);
+
+  const platforms = createPlatforms(engine);
+  player = createPlayer(engine);
+  const stars = createStars(engine, player);
+
+  engine.physics.add.collider(platforms, stars);
+  engine.physics.add.collider(player, platforms);
+
+  cursors = engine.input.keyboard.createCursorKeys();
+
+  scoreText = engine.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#000' });
+  fpsText = engine.add.text(600, 16, '60 fps', { fontSize: '32px', fill: '#000' });
+
+  bombs = engine.physics.add.group();
+
+  engine.physics.add.collider(bombs, platforms);
+  engine.physics.add.collider(player, bombs, player =>
+  {
+    this.physics.pause();
+
+    player.setTint(0xff0000);
+
+    player.anims.play('turn');
+
+    gameOver = true;
+  }, null, this);
+}
+
+function update()
+{
+  fpsText.setText(`${this.game.loop.actualFps.toFixed(2)}fps`);
+
+  if (gameOver) return;
+
+  if (cursors.left.isDown)
+  {
+    player.setVelocityX(-160);
+    player.anims.play('left', true);
+  }
+  else if (cursors.right.isDown)
+  {
+    player.setVelocityX(160);
+    player.anims.play('right', true);
+  }
+  else
+  {
+    player.setVelocityX(0);
+    player.anims.play('turn');
+  }
+
+  if (cursors.up.isDown && player.body.touching.down) player.setVelocityY(-330);
 }
